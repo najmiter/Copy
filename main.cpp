eec1cdadfd3da5914347520f624fd5d1ca4dbc72
Najam <85332859+najmiter@users.noreply.github.com>
2023-03-13 23:39:46 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,28 +2,48 @@
 #include <ctime>
 #include <cmath>
 #include <algorithm>
+#include <chrono>
+#include <cstring>
 
-const size_t SIZE{2000UL};
+const size_t SIZE{200000000UL};
 
 int main() {
-    int64_t my_array[SIZE]{};
+    double* my_array = new double[SIZE];
 
-    srand(time(nullptr));
-    for (auto& value : my_array) {
-        value = rand() % 1000;
-        std::cout << value << ' ';
+//    srand(time(nullptr));
+    for (auto i {0UL}; i < SIZE; ++i) {
+        my_array[i] = rand() % 1000;
+//        std::cout << value << ' ';
     }
 
     std::cout << "\n\n";
 
-    decltype(my_array) your_array{};
+    double* your_array = new double[SIZE];
 
-    copy<int64_t, std::size(my_array)>(your_array, my_array);
+    auto before = std::chrono::steady_clock::now();
+
+    copy<double, SIZE>(your_array, my_array);
+
+    auto after = std::chrono::steady_clock::now() - before;
+    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(after).count() << '\n';
 
 
-    for (auto& value : your_array) {
-        std::cout << value << ' ';
-    }
-    std::cout << "\n\n";
+    double* array1 = new double[SIZE];
+//    decltype(my_array) array2{};
+
+    auto before1 = std::chrono::steady_clock::now();
+
+    std::memcpy(array1, your_array, SIZE*sizeof (double));
+
+    auto after1 = std::chrono::steady_clock::now() - before1;
+    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(after1).count() << '\n';
+
+    delete[] my_array;
+    delete[] your_array;
+    delete[] array1;
+//    for (auto& value : your_array) {
+//        std::cout << value << ' ';
+//    }
+//    std::cout << "\n\n";
 
 }
